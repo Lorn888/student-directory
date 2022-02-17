59f8ced193ee093b9fbc667eab35500084698781
Patryk Okpara <patrykokpara@hotmail.com>
2022-02-17 14:13:32 +0000

    Exercise 2. Print only names starting with a letter b

--- directory-excersises.rb
+++ directory-excersises.rb
@@ -24,7 +24,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].downcase.start_with?("b")
+      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
